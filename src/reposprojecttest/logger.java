d0bc76e17a7ea8e6b7fdf18e24e8c270204de23f
sreevalli <kotapatisreevalli@gmail.com>
2021-10-06 19:35:56 +0530

    selenium learing added

--- src/reposprojecttest/logger.java
+++ src/reposprojecttest/logger.java
@@ -4,6 +4,7 @@ public class logger {
 	public static void main(String[] args) {
 System.out.println("hi");
  System.out.println("how r u");
- System.out.println("im sreevalli samanthapudi");
+ System.out.println("im sreevalli ");
+ System.out.println("i am learning selenium");
 	}
 }
